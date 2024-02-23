295e6ad1fbf15a5009a3487716892b2bff627931
Marcos Curtidor <3048652220116@ingenieria.usac.edu.gt>
2024-02-23 15:28:14 -0600

    commit 1

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+    public static void HelloWorld() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld();
+    }
+}
